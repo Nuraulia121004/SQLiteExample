ce2b9256c84aaceb53232ccfb449289323aff343
Nuraulia Octaviana <nurauliaaa12@gmail.com>
2024-11-12 12:22:34 +0700

    SQLite Example

---
+++ app/src/main/java/aulia/polbeng/ac/id/sqliteexample/StudentModel.kt
@@ -0,0 +1,12 @@
+package aulia.polbeng.ac.id.sqliteexample
+
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StudentModel (
+    val nim: String,
+    val name: String,
+    val age: String
+): Parcelable
